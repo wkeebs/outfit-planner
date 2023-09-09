5a00fe3b457cb8a7f5096b3aea58466eb47b60c3
Will Keeble <will.a.keeble@gmail.com>
2023-09-09 18:10:58 +1000

    Generate outfit styling

--- app/frontend/src/components/GenerateOutfit.jsx
+++ app/frontend/src/components/GenerateOutfit.jsx
@@ -1,14 +1,16 @@
 function GenerateOufit() {
   return (
     <body>
-      <div class="w-1/2 bg-white p-4">
-        <h2 class="text-2xl font-bold mb-4">Generated Outfit</h2>
-        <img
-          src="placeholder.jpg"
-          alt="Generated Outfit"
-          class="w-full rounded-lg"
-        ></img>
-        <button class="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+      <div class="bg-white p-4 h-screen flex-col justify-center items-center m-8 rounded-2xl shadow-xl hover:shadow-2xl">
+        <h1 class="text-3xl font-semibold mb-4 text-center">Generated Outfit</h1>
+        <div className="h-5/6">
+          <img
+            src="placeholder.jpg"
+            alt="Generated Outfit"
+            class="w-full rounded-lg"
+          ></img>
+        </div>
+        <button class="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 w-full">
           Generate Outfit
         </button>
       </div>
